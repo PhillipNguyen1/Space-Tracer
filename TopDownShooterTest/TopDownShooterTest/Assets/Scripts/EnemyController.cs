14378755b4fcff1afc0e8e2592c04b771b2e9a68
olcastan <o_castaneda@outlook.com>
2018-09-20 18:36:07 -0500

    ShooterTemplate

---
+++ TopDownShooterTest/TopDownShooterTest/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+
+    //make reference to enemy rigidbody so we can modify its velocity
+    private Rigidbody myRB;
+
+    public float moveSpeed;
+
+    //what is the enemy's target??
+    public PlayerController thePlayer;
+
+	// Use this for initialization
+	void Start () {
+        myRB = GetComponent<Rigidbody>(); //find rigidbody that's attached to enemy
+        thePlayer = FindObjectOfType<PlayerController>(); //self - explanatory
+
+
+
+	}
+    //**Note: when we want to move any object within our world that has a 'rigidbody' attached to it
+    //we want to do it in (FixedUpdate) function, rather than update.
+	
+
+    void FixedUpdate(){
+        myRB.velocity = (transform.forward * moveSpeed);
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(thePlayer.transform.position); //follows player's current position
+
+	}
+}
---
+++ TopDownShooterTest/TopDownShooterTest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed; //determines how fast player moves
+    private Rigidbody myRigidBody;  //apply forces so player can move around
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    //camera references
+    private Camera mainCamera;
+
+    public GunController theGun;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // More responsive movement
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        //camera to mouse interaction
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        //create another plane
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        //raycast (determine lenght of particular ray; if camera hits anything else in our world)
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            //make player look at what we (the mouse) is pointing at
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+
+        }
+
+        //left mouse button to fire
+        if (Input.GetMouseButtonDown(0))
+        {
+            theGun.isFiring = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            theGun.isFiring = false;
+        }
+	}
+
+    // apply input and speed to rigidbody component
+    void FixedUpdate() {
+        myRigidBody.velocity = moveVelocity;
+    }
+}
