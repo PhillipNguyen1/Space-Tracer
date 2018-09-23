476cddfe340f8db77759a7b2ecdca32dbb7ea543
olcastan <o_castaneda@outlook.com>
2018-09-23 15:22:53 -0500

    Stuff Added
    - Added crosshairs (mouse cursor not visible)
    - Added enemy hitbox (enemy can now damage)
    - Added player and enemy health (both objects will now be destroyed once health reaches 0)
    - Added player damage feedback (player flashes white)
    - Movement and player mass/physics has been refined

---
+++ TopDownShooterTest/TopDownShooterTest/Assets/Scripts/Crosshairs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshairs : MonoBehaviour {
+
+    //crosshairs will light up when hovered over enemy.
+    public LayerMask targetMask;
+    public SpriteRenderer dot;
+    public Color dotHighlightColor;
+    Color originalDotColor;
+
+    void Start()
+    {
+        originalDotColor = dot.color;
+        Cursor.visible = false;
+    }
+
+    void Update () {
+        //crosshair rotate effect
+        transform.Rotate(Vector3.forward * -40 * Time.deltaTime);
+	}
+    public void Detect(Ray ray)
+    {
+        if (Physics.Raycast(ray, 100, targetMask))
+        {
+            dot.color = dotHighlightColor;
+        }
+        else
+        {
+            dot.color = originalDotColor;
+        }
+    }
+}
---
+++ TopDownShooterTest/TopDownShooterTest/Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//how much damage are we going to do to the player?
+public class HurtPlayer : MonoBehaviour {
+
+    public int damageToGive; //can be used if player walks into other things that hurt him
+
+    //when player walks into hurtbox, what happens
+	public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(damageToGive);
+        }
+    }
+    
+}
